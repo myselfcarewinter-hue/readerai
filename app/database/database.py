fcaf3857e1d960b6511194abb59ad2f7d19f0fc2
katir velavan D.R <myselfcarewinter@gmail.com>
2026-06-17 13:45:27 +0530

    Add MongoDB Atlas support

--- app/database/database.py
+++ app/database/database.py
@@ -1,20 +1,20 @@
 from pymongo import MongoClient
+from dotenv import load_dotenv
 import os
 
+load_dotenv()
 
-MONGO_URL = os.getenv(
-    "MONGO_URL",
-    "mongodb://readerai_db:27017"
-)
+client = MongoClient(os.getenv("MONGO_URL"))
 
-client = MongoClient(MONGO_URL)
-
+try:
+    client.admin.command("ping")
+    print("MongoDB Atlas connected")
+except Exception as e:
+    print("MongoDB connection failed")
+    print(e)
 
 db = client["readerai"]
 
-
 documents = db["documents"]
-
 users = db["users"]
-
 chats = db["chats"]
